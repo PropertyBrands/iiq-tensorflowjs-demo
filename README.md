1ef5f01b566d88e50472a539fe57f8961f1e65b4
ethanhinson <ethan@bluetent.come>
2023-08-10 02:29:53 -0600

    Added a very basic object detection demo and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple TensorFlowJS Demo
+
+This is a simple demo of TensorFlowJS. It uses a pre-trained model to detect objects in images.
+
+## Running the demo
+
+- Clone this repo
+- Run `npm install`
+- Copy 6 images named `image1.jpg` to `image6.jpg` into this folder
+- Run `npm run watch`
+- Open `localhost:1234` in your browser
---
+++ index.js
@@ -0,0 +1,80 @@
+
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+
+import image1URL from './image1.jpg';
+import image2URL from './image2.jpg';
+import image3URL from './image3.jpg';
+import image4URL from './image4.jpg';
+import image5URL from './image5.jpg';
+import image6URL from './image6.jpg';
+
+let modelPromise;
+
+window.onload = () =>  {
+    modelPromise = cocoSsd.load();
+    predict();
+}
+
+
+const select = document.getElementById('base_model');
+select.onchange = async (event) => {
+    const model = await modelPromise;
+    model.dispose();
+    modelPromise = cocoSsd.load(
+        {base: event.srcElement.options[event.srcElement.selectedIndex].value});
+    predict();
+};
+
+function predict() {
+    const imgs = [
+        image1URL,
+        image2URL,
+        image3URL,
+        image4URL,
+        image5URL,
+        image6URL,
+    ]
+
+    const el = document.getElementById('images');
+    el.innerHTML = '';
+    imgs.map(async (img, i) => {
+        let wrapper
+        wrapper = document.createElement('div');
+        wrapper.classList.add('images-wrapper');
+        const imgEl = document.createElement('img');
+        imgEl.src = img;
+        imgEl.classList.add(`img-${i}`);
+        wrapper.appendChild(imgEl);
+
+        const canvas = document.createElement('canvas');
+        canvas.id = `canvas-${i}`;
+        canvas.width = 1280;
+        canvas.height = 853;
+        wrapper.appendChild(canvas);
+        const model = await modelPromise;
+        const result = await model.detect(imgEl);
+
+        el.appendChild(wrapper)
+
+        const c = document.getElementById(`canvas-${i}`);
+        const context = c.getContext('2d');
+        context.drawImage(imgEl, 0, 0);
+        context.font = '10px Arial';
+
+        console.log('number of detections: ', result.length);
+        for (let i = 0; i < result.length; i++) {
+            context.beginPath();
+            context.rect(...result[i].bbox);
+            context.lineWidth = 1;
+            context.strokeStyle = 'green';
+            context.fillStyle = 'green';
+            context.stroke();
+            context.fillText(
+                result[i].score.toFixed(3) + ' ' + result[i].class, result[i].bbox[0],
+                result[i].bbox[1] > 10 ? result[i].bbox[1] - 5 : 10);
+        }
+    });
+}
